82c842ffd2daeb97ca3d6854d67e2310bf77d26e
zou-xian-liang <1098421358@qq.com>
2017-08-29 18:54:38 +0800

    1

--- app/src/main/java/com/ds/rxandroid/MainActivity.java
+++ app/src/main/java/com/ds/rxandroid/MainActivity.java
@@ -9,6 +9,8 @@ import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends Activity {
 
@@ -18,12 +20,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e(TAG, Thread.currentThread().getName());
         testRx();
     }
 
     private void testRx() {
 
         //创建一个上游 Observable：被观察者
+        //emitter：用来发出事件的，onNext,onComplete,onError
         Observable<Integer> observable = Observable.create(new ObservableOnSubscribe<Integer>() {
             @Override
             public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
@@ -38,6 +42,7 @@ public class MainActivity extends Activity {
 
 
         //创建一个下游 Observer：观察者
+        //Disposable:用来阶段接收者，不在接收事件，如调用了该方法，则事件还是会继续发送，但是观察者不再接收
         Observer<Integer> observer = new Observer<Integer>() {
             @Override
             public void onSubscribe(Disposable d) {
@@ -59,7 +64,18 @@ public class MainActivity extends Activity {
                 Log.e(TAG, "complete");
             }
         };
+
+        Consumer consumer = new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) throws Exception {
+
+            }
+        };
         //建立连接，只有调用了这个接口，被订阅者和订阅者才能开始发送和接收消息
         observable.subscribe(observer);
+
+        observable
+                .subscribeOn(Schedulers.newThread())
+                .subscribe(observer);
     }
 }
